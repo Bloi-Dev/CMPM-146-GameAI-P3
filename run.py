ffc7e9cf605f8f2161b1c20abf0ca83ba7a98bec
Louie D. Hidalgo <ldhidalg@ucsc.edu>
2026-07-13 16:47:44 -0700

    resolved run.py

--- run.py
+++ run.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
                  'opponent_bots/defensive_bot.py',
                  'opponent_bots/production_bot.py']
 
-    maps = [21, 13, 24, 23, 7]
+    maps = [71, 13, 24, 56, 7]
 
     my_bot = 'behavior_tree_bot/bt_bot.py'
     show = len(sys.argv) < 2 or sys.argv[1] == "show"
